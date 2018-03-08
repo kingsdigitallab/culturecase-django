b6e70d5106ac904183502e97cd8420743d0f4ca5
k1217897 <geoffroy.noel@kcl.ac.uk>
2018-03-08 18:12:26 +0000

    Added settings files for vagrant, bower, git and travis. Also added culturecase xml dump from wordpress.

---
+++ .vagrant_provisioning/local_settings.py
@@ -0,0 +1,43 @@
+from .base import *  # noqa
+
+DEBUG = True
+
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': db_engine,
+        'NAME': 'culturecase',
+        'USER': 'culturecase',
+        'PASSWORD': 'culturecase',
+        'ADMINUSER': 'postgres',
+        'HOST': 'localhost'
+    },
+}
+
+# 10.0.2.2 is the default IP for the VirtualBox Host machine
+INTERNAL_IPS = ['0.0.0.0', '127.0.0.1', '::1', '10.0.2.2']
+
+SECRET_KEY = '12345'
+
+FABRIC_USER = ''
+
+# -----------------------------------------------------------------------------
+# Django Debug Toolbar
+# http://django-debug-toolbar.readthedocs.org/en/latest/
+# -----------------------------------------------------------------------------
+
+try:
+    import debug_toolbar  # noqa
+
+    INSTALLED_APPS = INSTALLED_APPS + ['debug_toolbar']
+    MIDDLEWARE += [
+        'debug_toolbar.middleware.DebugToolbarMiddleware']
+    DEBUG_TOOLBAR_PATCH_SETTINGS = True
+except ImportError:
+    pass
+
+LOGGING['loggers']['culturecase'] = {}
+LOGGING['loggers']['culturecase']['handlers'] = ['console']
+LOGGING['loggers']['culturecase']['level'] = logging.DEBUG
