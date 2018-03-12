0ebee7dbe26e3adc3c6fa1ac2f25f02b936584b4
k1217897 <geoffroy.noel@kcl.ac.uk>
2018-03-12 17:58:38 +0000

    Fixed culturecase_wagtail/migrations/0018 . The first migration after isntalling the project was crashing because the content_type records already existed and the loaddata tries to load with different ids.

--- culturecase_wagtail/migrations/0018_auto_20180311_1352.py
+++ culturecase_wagtail/migrations/0018_auto_20180311_1352.py
@@ -3,22 +3,36 @@
 from __future__ import unicode_literals
 
 from django.db import migrations
+from django.contrib.contenttypes.models import ContentType
 
 
 def load_initial_wordpress_content(apps, schema_editor):
     from django.core.management.commands import loaddata
     from django.core import management
-    management.call_command(
-        'loaddata',
-        'culturecase_wagtail/data/culturecase.wordpress.2018-03-06.json',
-        verbosity=0
-    )
+
+    if 1:
+        # remove all existing content types to avoid duplicate keys
+        for ct in ContentType.objects.all():
+            ct.delete()
+
+        management.call_command(
+            'loaddata',
+            'culturecase.wordpress.2018-03-06.json',
+            verbosity=0
+        )
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('culturecase_wagtail', '0017_homepageslide_text_color'),
+        ('sessions', '0001_initial'),
+        ('kdl_wordpress2wagtail', '0005_auto_20180227_2210'),
+        ('wagtaildocs', '0007_merge'),
+        ('wagtailusers', '0006_userprofile_prefered_language'),
+        ('wagtailadmin', '0001_create_admin_access_permissions'),
+        ('wagtailembeds', '0003_capitalizeverbose'),
+        ('wagtailsearch', '0003_remove_editors_pick'),
     ]
 
     operations = [
